2487511de08394bd705d54446985ff29cc512d36
Toddy Ezra <toddyezra98@gmail.com>
2016-09-11 23:41:16 +0700

    commit ketiga

--- app/src/main/java/id/sch/smktelkom_mlg/learn/pengembalianbuku/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/pengembalianbuku/MainActivity.java
@@ -4,7 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -13,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     EditText etKelas ;
     Button bOK;
     Spinner spJurusan;
+    RadioButton rbSes, rbSeb;
+    CheckBox cbBipa, cbBmtk,cbBkamus;
     TextView tvHasil;
 
     @Override
@@ -26,12 +30,17 @@ public class MainActivity extends AppCompatActivity {
         etKelas = (EditText) findViewById(R.id.editTextKelas);
         bOK = (Button) findViewById(R.id.buttonOK);
         spJurusan = (Spinner)findViewById(R.id.spinnerJurusan);
+        rbSes = (RadioButton) findViewById(R.id.radioButtonSes);
+        rbSeb = (RadioButton) findViewById(R.id.radioButtonSeb);
+        cbBipa= (CheckBox) findViewById(R.id.checkBoxIpa);
+        cbBmtk = (CheckBox) findViewById(R.id.checkBoxMtk);
+        cbBkamus = (CheckBox) findViewById(R.id.checkBoxKamus);
         tvHasil = (TextView) findViewById(R.id.textViewHasil);
         bOK.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                doProcess();
 
+                doProcess();
 
             }
         });
@@ -40,9 +49,26 @@ public class MainActivity extends AppCompatActivity {
     private void doProcess() {
         {
             if (isValid());
+
             String nama = etNama.getText().toString();
                  String kelas = etKelas.getText().toString();
-        tvHasil.setText(nama+" dari kelas "+kelas+" "+spJurusan.getSelectedItem().toString());
+            String hasil= "  Buku yang telah dikembalikan \n";
+            String hasil1= "Tanggal Check \n";
+
+            {
+                int startlen = hasil.length();
+                if (cbBipa.isChecked()) hasil+=cbBipa.getText()+"\n";
+                if (cbBmtk.isChecked()) hasil+=cbBmtk.getText()+"\n";
+                if (cbBkamus.isChecked()) hasil+=cbBkamus.getText()+"\n";
+
+                if (hasil.length()==startlen) hasil+="Tidak ada pilihan";
+            }
+            {
+                if (rbSes.isChecked()) hasil1+=rbSes.getText()+"\n";
+                if (rbSeb.isChecked()) hasil1+=rbSeb.getText()+"\n";
+            }
+
+        tvHasil.setText(nama+" dari kelas "+kelas+" "+spJurusan.getSelectedItem().toString()+",   "+hasil+hasil1);
     }
 }
 
@@ -50,6 +76,9 @@ public class MainActivity extends AppCompatActivity {
         boolean valid = true;
         String nama = etNama.getText().toString();
         String kelas = etKelas.getText().toString();
+        String hasil= "Buku yang telah dikembalikan \n";
+
+
 
         if (nama.isEmpty())
         {
@@ -63,12 +92,18 @@ public class MainActivity extends AppCompatActivity {
 
         if (kelas.isEmpty())
         {
-            etKelas.setError("kelas belom diisi");
+            etKelas.setError("kelas belum diisi");
         }
         else
         {
             etKelas.setError(null);
         }
+
+
+
+
+
+
         return false;
 
     }
